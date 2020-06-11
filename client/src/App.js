4fc862074eb769d2007268785bf2de29439862f5
leodotng <josh@leo.ng>
2020-06-11 10:13:48 -0600

    connect react client to backend express

--- client/src/App.js
+++ client/src/App.js
@@ -3,22 +3,25 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+
+  }
+  callAPI() {
+    fest("http://localhost:9000/testAPI")
+    .then(res => res.text())
+    .then(res => this.setState({ apiResponse: res }));
+    .catch(err => err);
+  }
+
+  componenentWillMount() {
+    this.callAPI();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p className="App-intro">;{this.state.apiResponse}</p>
     </div>
   );
 }
