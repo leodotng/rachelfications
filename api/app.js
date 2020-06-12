78c422f00468745ef8a463a263ec9f19f1755d63
leodotng <josh@leo.ng>
2020-06-11 18:07:06 -0600

    updated routes to pull data from backend

--- api/app.js
+++ api/app.js
@@ -7,7 +7,7 @@ var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var testAPIRouter = require('./routes/testAPI');
+var getList = require('./routes/getList');
 var app = express();
 
 // view engine setup
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/testAPI', testAPIRouter);
+app.use('/getList', getList);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
